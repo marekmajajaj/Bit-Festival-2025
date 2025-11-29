6b7446f6236aea92c2fe712f24cd863f527c3d34
Lightpen-cpu <52523769+Lightpen-cpu@users.noreply.github.com>
2025-11-29 19:18:45 +0100

    kolizja kamery

---
+++ Assets/Scrpits/CameraSensitivity.cs
@@ -0,0 +1,25 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraSensitivity : MonoBehaviour
+{
+    /*public CinemachineFreeLook freeLookCamera;
+
+
+    void Start()
+    {
+        // ustaw pocz¹tkow¹ wartoœæ Gain
+        UpdateSensitivity(GlobalVariables.sensitivity);
+    }
+
+    void UpdateSensitivity(float value)
+    {
+        // Zmiana GAIN dla osi X
+        freeLookCamera.m_XAxis.m_MaxSpeed = value;
+        Debug.Log(freeLookCamera.m_XAxis.m_MaxSpeed);
+        // Zmiana GAIN dla osi Y
+        freeLookCamera.m_YAxis.m_MaxSpeed = value;
+    }*/
+    // Update is called once per frame
+
+}
