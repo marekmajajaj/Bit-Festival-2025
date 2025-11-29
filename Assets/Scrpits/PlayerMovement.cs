4b8308de0a00ee86adf544b1c560bfa243517150
Lightpen-cpu <52523769+Lightpen-cpu@users.noreply.github.com>
2025-11-29 19:28:46 +0100

    asd

--- Assets/Scrpits/PlayerMovement.cs
+++ Assets/Scrpits/PlayerMovement.cs
@@ -110,7 +110,6 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-<<<<<<< Updated upstream
 
         float checkDistance = 0.51f;   // jak daleko szukać ziemi w dół
         float offset = 0.5f;         // połowa szerokości obiektu (dla 1x1 ≈ 0.5, ale trochę mniej)
